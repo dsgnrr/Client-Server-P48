b198373eb5368d802412925f978a70c4e3579674
Semeniuk Oleg <semeniuk.oleg2002@gmail.com>
2026-05-07 18:46:22 +0300

    lesson 4

---
+++ Lesson3-UDP(Broadcast)/UDPClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using UdpClient client = new();
+            string serverHost = "10.3.1.7";
+            int port = 3000;
+
+            try
+            {
+
+                byte[] request = Encoding.UTF8.GetBytes(Console.ReadLine());
+                int bytes = await client.SendAsync(request, request.Length, serverHost, port);
+
+                UdpReceiveResult result = await client.ReceiveAsync();
+                Console.WriteLine($"Server[{result.RemoteEndPoint}]->{Encoding.UTF8.GetString(result.Buffer)}");
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Error connect to server: {0}", ex.Message);
+            }
+        }
+    }
+}
---
+++ Lesson3-UDP(Broadcast)/UDPServer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPServer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int port = 3000;
+            //using UdpClient server = new UdpClient(
+            //    new IPEndPoint(
+            //        IPAddress
+            //        .Parse("10.3.1.7"), 
+            //        port)
+            //    );
+
+            using UdpClient server = new UdpClient(port);
+
+            while (true)
+            {
+                try
+                {
+                    UdpReceiveResult result = await server.ReceiveAsync();
+                    byte[] recieveResult = result.Buffer;
+
+                    Console.WriteLine($"{result.RemoteEndPoint} -> {Encoding.UTF8.GetString(recieveResult)}");
+
+                    byte[] response = Encoding.UTF8.GetBytes("Hello from server");
+
+               
+                    await server.SendAsync(
+                        response,
+                        response.Length,
+                        result.RemoteEndPoint);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Can`t send response, because is dead");
+                }
+            }
+        }
+    }
+}
